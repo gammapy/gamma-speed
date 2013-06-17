f8efd47b77e5e3b571b1debbf76c310d1ab02962
ignatndr <ignatndr@gmail.com>
2013-06-17 20:12:16 +0200

    First plotting tool

--- code/monitor.py
+++ code/monitor.py
@@ -5,23 +5,60 @@ Example: ./monitor.py "ls -lh"
 """
 import argparse
 import subprocess
+from subprocess import PIPE
 import psutil
+import csv
+import matplotlib.pyplot as plt
 
 def monitor(cmd, outfile):
-    """Execute a given command (cmd is a string)
-    and write usage to outfile"""
-    popen = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
-    #import IPython; IPython.embed()
-    process = psutil.Process(popen.pid)
+    	"""Execute a given command (cmd is a string)
+    	and write usage to outfile"""
+    	process = psutil.Popen(cmd[0].split(), stdout=subprocess.PIPE)
+    	info = dict(disk_io=[],cpu_usage=[],mem_usage=[],name=process.name)
+    
+	#The following thread stops when the initial one has come to a halt.
+    	while process.poll()==None:
+		try:
+			info['disk_io'].append(process.get_io_counters ())
+			info['cpu_usage'].append(process.get_cpu_percent(interval=0.0001))
+            		info['mem_usage'].append(process.get_memory_info())
+        	except psutil.AccessDenied:
+			print 'Process is over'
+	
+    
+    	#write the values into a casv file	    
+    	writer = csv.writer(open(outfile, 'wb'))    
+	for key, value in info.items():
+   		writer.writerow([key, value])
+   	
+    	#import IPython; IPython.embed()
+    	fig=plt.figure(1)
+	
+	plt.subplot(411)
+	plt.plot(info['cpu_usage'])
+    	plt.ylabel('CPU (%)')
+    	plt.title('CPU usage for ' + info['name'])
+    	
+	plt.subplot(412)
+	plt.plot(info['mem_usage'][0])
+	plt.ylabel('Memory (bytes)')
+	plt.title('Memory usage for' + info['name'])
+	
+	plt.subplot(413)
+	plt.plot(info['disk_io'][3])
+	plt.ylabel('Read (bytes)')
+	plt.title('Bytes read by' + info['name'])
 
-    while True:
-        info = dict()
-        info['cpu_usage'] = process.get_cpu_percent(interval=0.01)
-        info['mem_usage'] = 42
-        info['disk_io'] = 42
-        print info
-        # TODO: finish this script
-
+	plt.subplot(414)
+        plt.plot(info['disk_io'][4])
+        plt.ylabel('Write (bytes)')
+        plt.title('Bytes written by' + info['name'])
+	
+	fig.subplots_adjust(hspace=.5)
+	
+	plt.show()
+    
+    
 def main():
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('-o', '--outfile', default='monitor.csv',
