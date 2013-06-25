d41d66f3cbef47c008c492a420b228fd99dde74f
ignatndr <ignatndr@gmail.com>
2013-06-25 17:23:11 +0200

    Improved thread control (unfinished)

--- code/monitor.py
+++ code/monitor.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-"""Monitor CPU, memory and disk I/O for a given process.
+"""Automatically Monitor CPU, memory and disk I/O for a given process.
 
 Example: ./monitor.py "ls -lh"
 """
@@ -7,21 +7,24 @@ import argparse
 import pandas as pd
 import psutil
 import subprocess
+import time
+import multiprocessing
+import os
 
 def monitor(cmd, outfile):
         """Execute a given command (cmd is a string)
         and write usage to outfile"""
         process = psutil.Popen(cmd[0].split(), stdout=subprocess.PIPE)
 
-        df = pd.DataFrame(columns=['CPU_USAGE', 'MEM_USAGE', 'IO_READ_COUNTS', 'IO_WRITE_COUNTS', 'IO_WRITE_BYTES', 'PROCESS_NAME'])
+        df = pd.DataFrame(columns=['CPU_USAGE', 'MEM_USAGE', 'IO_READ_COUNTS', 'IO_WRITE_COUNTS', 'IO_WRITE_BYTES', 'PROCESS_NAME','TIME'])
                 
         name = process.name
         # The following thread stops when the initial one has come to a halt.
         while process.poll() == None:
             try:
                 s = pd.Series([process.get_cpu_percent(interval=0.1), process.get_memory_info()[1], 
-                               process.get_io_counters ()[0], process.get_io_counters ()[1], process.get_io_counters ()[3], name],
-                              index=['CPU_USAGE', 'MEM_USAGE', 'IO_READ_COUNTS', 'IO_WRITE_COUNTS', 'IO_WRITE_BYTES', 'PROCESS_NAME'])
+                               process.get_io_counters ()[0], process.get_io_counters ()[1], process.get_io_counters ()[3], name,time.time()],
+                              index=['CPU_USAGE', 'MEM_USAGE', 'IO_READ_COUNTS', 'IO_WRITE_COUNTS', 'IO_WRITE_BYTES', 'PROCESS_NAME','TIME'])
                 df = df.append(s, ignore_index=True)
             except psutil.AccessDenied:
                 print 'Process is over'
@@ -30,13 +33,18 @@ def monitor(cmd, outfile):
    
 def main():
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('-o', '--outfile', default='monitor.csv',
+    parser.add_argument('-o', '--outfile', default='monitor',
                         help='Output file name')
+    parser.add_argument('-mt', '--maxthreads', default=multiprocessing.cpu_count(),
+                        help='Maximum number of threads for the measurement')
     parser.add_argument('cmd', nargs='+',
                         help='Command to execute')
 
     args = parser.parse_args()
-    monitor(cmd=args.cmd, outfile=args.outfile)
+#     import IPython; IPython.embed()
+    for i in xrange(args.maxthreads):
+        os.environ["OMP_NUM_THREADS"]=str(i+1)
+        monitor(cmd=args.cmd, outfile=args.outfile+"_CPUs="+str(i+1)+"_.csv")
     
 
 if __name__ == '__main__':
--- code/monitorPlot.py
+++ code/monitorPlot.py
@@ -1,60 +1,64 @@
 #!/usr/bin/env python
-"""
-Plotting tool for Monitor CPU, memory and disk I/O for a given process.
-
-Example: ./monitor.py "ls -lh"
-
-"""
 import argparse
 import matplotlib.pyplot as plt
 import pandas as pd
+from matplotlib.ticker import FormatStrFormatter
+import multiprocessing
 
-def monitorPlot(infile):
-        # write the values into a csv file        
-        df = pd.read_csv(infile)
+def monitorPlot(infile,nfiles):
+        # read the values into a csv file        
+        fig=plt.figure(1)
+        for i in xrange(nfiles):
+            df = pd.read_csv(infile+"_CPUs="+str(i+1)+"_.csv")
+            name = df.at[1, 'PROCESS_NAME']
         
-        name = df.at[1, 'PROCESS_NAME']
-        fig = plt.figure(1)
-     
-        plt.subplot(211)
-        df['CPU_USAGE'].plot()
-        plt.ylabel('CPU (%)')
-        plt.title('CPU usage for ' + name)
-         
-        plt.subplot(212)
-        df['MEM_USAGE'].plot()
-        plt.ylabel('RAM usage (bytes)')
-        plt.title('Memory usage for ' + name)
-     
-        fig = plt.figure(2)
-        plt.subplot(311)
-        df['IO_READ_COUNTS'].plot()
-        plt.ylabel('IO Read (counts)')
-        plt.title('IO Read counts for ' + name)
- 
-        plt.subplot(312)
-        df['IO_WRITE_COUNTS'].plot()
-        plt.ylabel('IO Write (counts)')
-        plt.title('IO write counts for ' + name)
         
-        plt.subplot(313)
-        df['IO_WRITE_BYTES'].plot()
-        plt.ylabel('IO Write (bytes)')
-        plt.title('Bytes written by ' + name)
-     
+            df['IO_WRITE_BYTES']=df['IO_WRITE_BYTES'].diff()/df['TIME'].diff()/int(1e6)
+            df['MEM_USAGE']=df['MEM_USAGE']/int(1e6)
+        
+            df['TIME']=df['TIME']-df.at[0,'TIME']
+        
+            plt.subplot(311)
+            df.plot(x='TIME',y='CPU_USAGE')
+            plt.ylabel('CPU (%)')
+            plt.title('CPU usage for ' + name)
+            plt.gca().xaxis.set_major_formatter(FormatStrFormatter('%.2f s'))
+        
+            plt.subplot(312)
+            df.plot(x='TIME',y='MEM_USAGE')
+            plt.ylabel('RAM usage (MB)')
+            plt.title('Memory usage for ' + name)
+            plt.gca().xaxis.set_major_formatter(FormatStrFormatter('%.2f s'))
+#         fig = plt.figure(2)
+#         plt.subplot(311)
+#         df['IO_READ_COUNTS'].plot()
+#         plt.ylabel('IO Read (counts)')
+#         plt.title('IO Read counts for ' + name)
+#  
+#         plt.subplot(312)
+#         df['IO_WRITE_COUNTS'].plot()
+#         plt.ylabel('IO Write (counts)')
+#         plt.title('IO write counts for ' + name)     
+            plt.subplot(313)
+            df.plot(x='TIME',y='IO_WRITE_BYTES')
+            plt.ylabel('IO Write (MB/s)')
+            plt.title('Write rate ' + name)
+            plt.gca().xaxis.set_major_formatter(FormatStrFormatter('%.2f s'))
+
         fig.subplots_adjust(hspace=.5)
      
         plt.show()
     
 def main():
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('-i', '--infile', default='monitor.csv',
-                        help='Input file name')
+    parser.add_argument('-i', '--infile', default='monitor',
+                        help='Input file name - prefix')
+    parser.add_argument('-n', '--nrcsv', default=multiprocessing.cpu_count(),
+                        help='Number of csv files')
     
     args = parser.parse_args()
-    monitorPlot(infile=args.infile)
+    monitorPlot(infile=args.infile,nfiles=args.nrcsv)
     
 
 if __name__ == '__main__':
     main()
-
