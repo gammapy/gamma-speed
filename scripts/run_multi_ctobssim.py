c244aa900e5fd791d5b719cd77857d06fcee42ca
ignatndr <ignatndr@gmail.com>
2013-06-25 14:32:23 +0200

    Added command line options for run_multi_ctobssim.py

--- scripts/run_multi_ctobssim.py
+++ scripts/run_multi_ctobssim.py
@@ -20,7 +20,7 @@ def set(RA=83.63, DEC=22.01, tstart=0.0, duration=1800.0, deadc=0.95,
     # Set pointing direction
     pntdir = gammalib.GSkyDir()
     pntdir.radec_deg(RA, DEC)
-    # import IPython; IPython.embed(); 1/0
+
     pnt = gammalib.GCTAPointing()
     pnt.dir(pntdir)
     obs.pointing(pnt)
@@ -65,30 +65,48 @@ def set(RA=83.63, DEC=22.01, tstart=0.0, duration=1800.0, deadc=0.95,
     return obs
 
 
-def run_multi_ctobssim(obstime=1e5):
+def run_multi_ctobssim(altRA, altDEC, altTSTART, altDURATION, altDEADC, altEMIN, altEMAX, altRAD, altIRF, altCALDB, outfile, nobs):
     """TODO: document what it does"""
-    obs1 = set(duration=int(obstime))
-    obs2 = set(duration=int(obstime))
-    observations = gammalib.GObservations()
-    obs1.id('1')
-    obs2.id('2')
-    observations.append(obs1)
-    observations.append(obs2)
-    observations.models('$CTOOLS/share/models/crab.xml')
 
+    observations = gammalib.GObservations()
+    
+    # Automatically generate a number of nobs
+    for i in xrange(nobs):
+        obs = set(RA=altRA, DEC=altDEC, tstart=altTSTART, duration=altDURATION, deadc=altDEADC,
+                  emin=altEMIN, emax=altEMAX, rad=altRAD,
+                  irf=altIRF, caldb=altCALDB)
+        obs.id(str(i))
+        observations.append(obs)
+
+    
+    observations.models('$CTOOLS/share/models/crab.xml')
+#         import IPython; IPython.embed(); 
+    
     ctobssim = ctools.ctobssim(observations)
     ctobssim.logFileOpen()
-    ctobssim['outfile'].filename('sim_events.xml')
+    ctobssim['outfile'].filename(outfile)
 
+    
     ctobssim.execute()
-    ctobssim.save()
+
 
 if __name__ == '__main__':
-    # TODO: set obstime, n_obs, write_outfiles, ... via command line arguments
-    parser = argparse.ArgumentParser(description=__doc__)
-    # parser.add_argument('outfile', type=str,
-    #                    help='Output XML file name')
+    parser = argparse.ArgumentParser(description=__doc__, add_help=True)
+    
+    parser.add_argument("-RA", type=float, help="right ascension", default=86.63)
+    parser.add_argument("-DEC", type=float, help="declination", default=22.01)
+    parser.add_argument("-tstart", type=float, help="the time at which the obsrvation starts", default=0.0)
+    parser.add_argument("-dur", type=float, help="duration of the observation", default=1800.0)
+    parser.add_argument("-deadc", type=float, help="deadtime correction factor", default=0.95)
+    parser.add_argument("-emin", type=float, help="minimum energy in TeV", default=0.1)
+    parser.add_argument("-emax", type=float, help="maximum energy in TeV", default=100.0)
+    parser.add_argument("-rad", type=float, help=" ", default=5.0)
+    parser.add_argument("-irf", type=str, help="name of the file containing the Instrument Response Function", default="cta_dummy_irf")
+    parser.add_argument("-caldb", type=str, help="path to calibration database", default="$GAMMALIB/share/caldb/cta")
+    parser.add_argument("-outfile", type=str, help="name of outfile", default='sim_events.xml')
+    parser.add_argument("-nobs", type=int, help="number of observations to be generated", default=1)
+# TODO: for some reason, the print_help() and implicitly, the -h or --help options do not work
+#     parser.print_help()
     args = parser.parse_args()
-    args = vars(args)
 
-    run_multi_ctobssim()
+    run_multi_ctobssim(args.RA, args.DEC, args.tstart, args.dur, args.deadc, args.emin, args.emax, args.rad, args.irf, args.caldb, args.outfile, args.nobs)
